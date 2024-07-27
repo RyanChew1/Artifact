70c46e7c5bb1204c518c746343c63350f9df338e
“RyanChew1” <chewryan0@gmail.com>
2024-07-26 22:51:23 -0700

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from '@/components/mode-toggle'
+
+const RootLayout = () => {
+  return (
+    <ThemeProvider storageKey="vite-ui-theme">
+      {/* Theme Switcher Toggle */}
+      <div  className="m-5 absolute z-10 flex right-10">
+        <ModeToggle/>
+      </div>
+      RootLayout
+    </ThemeProvider>
+    
+  )
+}
+
+export default RootLayout
